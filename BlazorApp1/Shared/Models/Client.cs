ecf76d2a635708e6611dab527e35074dc88a37bc
Richard <xqtwkk@gmail.com>
2022-12-05 07:04:29 +0300

    save 2: did something?

--- BlazorApp1/Shared/Models/Client.cs
+++ BlazorApp1/Shared/Models/Client.cs
@@ -10,15 +10,19 @@ namespace BlazorApp1.Shared.Models
     public class Client
     {
         public int ClientId { get; set; }
-        [Required]
-        [StringLength(100)]
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(100, ErrorMessage = "Name is too long.")]
         public string ClientName { get; set; } = null!;
         [Required]
         [StringLength(100)]
         public string ClientSurname { get; set; } = null!;
         [Required] [StringLength(500)]
         public string Address { get; set; } = null!;
+        [Required]
+        [Phone]
         public string CellNumber { get; set; } = null!;
+        [Required]
+        [EmailAddress]
         public string EmailId { get; set; } = null!;
 
         public string FullName
